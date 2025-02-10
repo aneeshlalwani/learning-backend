8dd7f43a98a8bf08338e5ede0907658195d4fbd6
aneeshlalwani <aneeshlalwani945@gmail.com>
2025-02-10 05:01:13 +0500

    feat: implement cloudinary utility

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,3 +8,22 @@ cloudinary.config({
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        // Upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // File has been uploaded successfully
+        console.log("File has been uploaded successfully", response.url);
+        return response
+    } catch (error) {
+        // Remove the locally saved temporary file as the upload operation gets failed
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
