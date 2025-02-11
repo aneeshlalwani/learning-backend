0459142c538b95a2b1aff4a909b569b44076662d
aneeshlalwani <aneeshlalwani945@gmail.com>
2025-02-12 02:29:54 +0500

    chore: add structured comments

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,39 +4,34 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 const registerUser = asyncHanlder(async (req, res) => {
-  // user user details from frontend
-  // Validate Data
-  // Create User
-  // Check for image, check for avatar
-  // upload them to cloudinary, avatar
-  // create user object - create entry in db
-  // remove password and refresh token field from response
-  // check for user creation
-  // return success response
+ 
+  // get user details from frontend
   const { fullName, username, email, password } = req.body;
   if ([fullName, username, email, password].some((field)=> field?.trim()==="")) {
     throw new ApiError(400, "All fields are required!");
   }
+  // Validate Data
   const existtedUser = User.findOne({
     $or: [{ username }, { email }]
   })
   if (existtedUser) {
     throw new ApiError(409, "Username or Email already exist!");
   }
-
+  // Check for images
   const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
+  // check for avatar
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required!");
   }
+  // upload images to cloudinary, avatar
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
     throw new ApiError(400, "Avatar upload failed!");
   }
-  
+  // create user object - create entry in db
   const user = await User.create({
     fullName,
     avatar: avatar.url,
@@ -45,15 +40,15 @@ const registerUser = asyncHanlder(async (req, res) => {
     password,
     username: username.toLowerCase()
   })
-
+  // check for user creation
   const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
+    "-password -refreshToken" // remove password and refresh token field from response
   )
 
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong while creating an user!");
   }
-
+  // return success response
   return res.status(201).json(
     new ApiResponse(200, createdUser, "User created successfully!")
   )
