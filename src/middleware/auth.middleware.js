3521527369ab68a451249b5b883dbc1b91defc3e
aneeshlalwani <aneeshlalwani945@gmail.com>
2025-02-17 23:57:51 +0500

    feat: implement middleware to verify jwt

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import { asynHanlder } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+export const verifyJWT = asynHanlder((req, res, next) => {
+  try {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+  
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request!");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = User.findById(decodedToken?._id).select("-password -refreshToken");
+  
+    if (!user) {
+      //  TODO: Discuss about frontend
+      throw new ApiError(404, "User not found!");
+    }
+      req.user = user;
+      next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Unauthorized request!");
+  }
+})
