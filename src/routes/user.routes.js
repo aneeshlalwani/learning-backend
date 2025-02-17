bb073a93e5614e2a5dee41317861ed308912ddbd
aneeshlalwani <aneeshlalwani945@gmail.com>
2025-02-17 23:59:47 +0500

    feat: add login route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js"
 const router = Router();
 
@@ -14,4 +14,5 @@ router.route("/register").post(upload.fields([
   }
 ]), registerUser)
 
+router.route("/login").post(loginUser)
 export default router;
